ae816a23d54130e49e78f7537418aad819971f84
Luis Serna <louieserna44@yahoo.com>
2025-09-16 08:44:53 -0500

    finished add transaction component

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import {ref, defineEmits} from 'vue'
+    const text = ref('')
+    const amount = ref('')
+
+    const emit = defineEmits([
+        'transactionSubmitted'
+    ])
+
+    const onSubmit = () =>{
+        const transactionDate = {
+            text: text.value,
+            amount: parseFloat(amount.value),
+
+        }
+
+        emit('transactionSubmitted', transactionDate)
+
+        text.value = ''
+        amount.value = ''
+    }
+
+</script>
+
+<template>
+<h3>Add a new Transaction</h3>
+<form id="form" @submit.prevent="onSubmit">
+    <div class="form-control">
+        <label for="text">Enter Transaction</label>
+        <input type="text" id="text" v-model="text" placeholder="Enter Transaction..."></input>
+    </div>
+     <div class="form-control">
+        <label for="amount">Enter Transaction Amount</label>
+        <input type="text" id="amount" v-model="amount" placeholder="Enter Negative Value for Expenses"></input>
+    </div>
+    <button class="btn">Add Transaction</button>
+</form>
+
+
+</template>
