1158eecf8d8a6e1e1ca26c82d3758e6ba52615c2
Luis Serna <louieserna44@yahoo.com>
2025-09-16 09:32:49 -0500

    completed expense tracker

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import AddTransaction from './components/AddTransaction.vue';
   import TransactionList from './components/TransactionList.vue';
-  import {computed, ref} from 'vue'
+  import {computed, ref, onMounted} from 'vue'
 
   const transactions = ref([])
 
@@ -36,11 +36,27 @@
     amount: transactionData.amount,
 
     })
+    saveToLocalStorage()
   }
 
   const handleDelete = (id) => {
     transactions.value = transactions.value.filter((x) => x.id !== id)
+    saveToLocalStorage()
   }
+
+  const saveToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value))
+  }
+
+  onMounted( () => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+
+    if(savedTransactions){
+      transactions.value = savedTransactions
+    }
+  })
+
+
 </script>
 
 
